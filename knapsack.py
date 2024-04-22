abe0358d49ae519233b725d1399f6580959ef1a6
NWhitener <nathanpwhitener@gmail.com>
2024-04-22 14:25:21 -0400

    function to read and fill the knapsack

--- knapsack.py
+++ knapsack.py
@@ -1,16 +1,23 @@
 #imports needed for the assignment 
 
-import numpy 
+import numpy as np
 import pandas 
 
-def knapsackIE(filePath): 
+def knapsackFiller(filePath):
+   
     file = open(filePath, 'r')
     weight = file.readline().split()[1]
     print("The weight of the knapsack is: ", weight)
     numItems = file.readline().split()[1]
     print("The number of items in the file is: ", numItems)
+    knapsack = np.zeros((int(numItems), 2))
+    print(knapsack)
     #this is a test loop    
-
+    for i in range(0,int( numItems)): 
+        line = file.readline()
+        knapsack[i][0] = int(line.split()[1])
+        knapsack[i][1] = int(line.split()[3])
+    print(knapsack)
 
 
 
@@ -24,7 +31,7 @@ def knapsackIE(filePath):
 
 
 def main():
-    knapsackIE('small.txt')
+    knapsackFiller('small.txt')
 
 if __name__ == '__main__': 
     main()
