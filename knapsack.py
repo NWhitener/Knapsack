14eab095d02cc11f5627580441a2b3ab84e5dedf
NWhitener <nathanpwhitener@gmail.com>
2024-04-22 14:08:10 -0400

    changes in the inital file

--- knapsack.py
+++ knapsack.py
@@ -3,3 +3,28 @@
 import numpy 
 import pandas 
 
+def knapsackIE(filePath): 
+    file = open(filePath, 'r')
+    weight = file.readline().split()[1]
+    print("The weight of the knapsack is: ", weight)
+    numItems = file.readline().split()[1]
+    print("The number of items in the file is: ", numItems)
+    #this is a test loop    
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    knapsackIE('small.txt')
+
+if __name__ == '__main__': 
+    main()
