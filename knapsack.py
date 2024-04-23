43126acc1bff1b900fd7712fd9e230c3734b69f3
NWhitener <nathanpwhitener@gmail.com>
2024-04-23 14:06:53 -0400

    Finished Problem 2

--- knapsack.py
+++ knapsack.py
@@ -49,7 +49,31 @@ def knapsackE(weight, items, sack):
 
     return dpTable[weight][0]
 
-
+def knapsackBack(weight, items, sack): 
+    #Base Cases: Knapsack empty  
+    dpTable = np.zeros((weight+1,items+1), dtype = int)
+    for j in range(0, items):
+        for w in range(0, weight+1):
+            if sack[j][0] > w:
+                dpTable[w][j] = dpTable[w][j-1]
+            else:
+                sackValue = sack[j][0] 
+                dpTable[w][j] = max(dpTable[w][j-1], dpTable[(w-sackValue)][j-1]+sack[j][1])
+    print(dpTable)
+    #Backtracking 
+    itemsList = []
+    weightLoc = weight
+    for j in range(items-1, -1,-1):         
+        if (j != 1): 
+            if dpTable[weightLoc][j-1] != dpTable[weightLoc][j]: 
+                print(dpTable[weightLoc][j], dpTable[weightLoc][j-1], j)
+                itemsList.append(j+1)
+                weightLoc = weightLoc - sack[j][0]
+        else:
+            if dpTable[weightLoc][j] != 0:
+                itemsList.append(j)
+                break
+    return itemsList           
 
 
 
@@ -59,11 +83,11 @@ def knapsackE(weight, items, sack):
 
 def main():
    start = time.time()
-   (weight, items, knapsack)=  knapsackFiller('small.txt')
+   (weight, items, knapsack)=  knapsackFiller('medium.txt')
    end = time.time()
    print("Filling took :", end - start)
    start2 = time.time()
-   print(knapsackE(weight, items, knapsack))
+   print(knapsackBack(weight, items, knapsack))
    end2 = time.time()
    print("Computing took: ", end2-start2)
 if __name__ == '__main__': 
